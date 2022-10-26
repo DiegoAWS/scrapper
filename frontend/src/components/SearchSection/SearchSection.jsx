3b8f71568d5dc83b0e778ada885dcc97e8ebca6e
Diego Escobar <diego.developer@yahoo.com>
2022-10-27 00:29:20 +0300

    Updated search view

--- frontend/src/components/SearchSection/SearchSection.jsx
+++ frontend/src/components/SearchSection/SearchSection.jsx
@@ -1,13 +1,81 @@
-import React from 'react'
-import TextField from '@mui/material/TextField';
-import './SearchSection.scss'
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import "./SearchSection.scss";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+import { useRef } from "react";
 
 function SearchSection() {
+  const [textEntry, setTextEntry] = useState("");
+  const [searchTerms, setSearchTerms] = useState([]);
+
+  const inputTextRef = useRef();
+
+  const handleAddSearchTerm = (text) => {
+    inputTextRef?.current?.focus();
+    setSearchTerms((data) => [...data, text]);
+    setTextEntry("");
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter" && textEntry !== "") {
+      handleAddSearchTerm(textEntry);
+    }
+  };
+
+  const handleRemoveSearchTerm = (index) => {
+    setSearchTerms((data) => data.filter((_, i) => i !== index));
+  };
+
   return (
-    <div className='searchSectionContainer'>
-        <TextField label="Outlined" variant="outlined" />
+    <div className="searchSectionContainer">
+      <div className="searchSection">
+        {searchTerms.map((searchTerm, index) => (
+          <TextField
+            key={index}
+            className="searchTerm"
+            variant="outlined"
+            fullWidth
+            value={searchTerm}
+            margin="normal"
+            InputProps={{
+              endAdornment: (
+                <IconButton
+                  color="error"
+                  onClick={() => handleRemoveSearchTerm(index)}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              ),
+            }}
+          />
+        ))}
+      </div>
+
+      <div className="addSearchTermSection">
+        <TextField
+          value={textEntry}
+          onChange={(e) => setTextEntry(e.target.value)}
+          className="addSearchTerm"
+          variant="outlined"
+          fullWidth
+          autoFocus
+          label="Add Search Term"
+          inputRef={inputTextRef}
+          onKeyPress={handleKeyPress}
+        />
+
+        <IconButton
+          color="primary"
+          disabled={textEntry.length === 0}
+          onClick={() => handleAddSearchTerm(textEntry)}
+        >
+          <PlaylistAddIcon fontSize="large" />
+        </IconButton>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SearchSection
+export default SearchSection;
